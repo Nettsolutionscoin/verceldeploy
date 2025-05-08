786da38f2a0f428efbc9b451a82464897ecbf840
Nettsolututionscoin <nettcoin@gmail.com>
2025-05-08 18:58:13 +0530

    Simple add

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to Jai Aanand
+          Edit <code>src/App.jsx</code> and Keep it simple
         </p>
       </div>
       <p className="read-the-docs">
