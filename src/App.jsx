04d5bf313c61fa69505c1f7b7b6cdd8deb3c86e0
Nettsolututionscoin <nettcoin@gmail.com>
2025-05-31 21:46:10 +0530

    A1

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and Keep it simpleee
+          Edit <code>src/App.jsx</code> and Keep it simple
         </p>
       </div>
       <p className="read-the-docs">
